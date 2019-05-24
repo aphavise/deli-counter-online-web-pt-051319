11ca5ff8f5ec67af47daec020e7aad5f4c84d6eb
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-05-24 00:53:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 
-def line(deli)
-  if deli.empty?
+def line(katz_deli)
+  if katz_deli.empty?
     puts "The line is currently empty."
   else  
     current_line = "The line is currently:"
